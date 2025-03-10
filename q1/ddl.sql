24aaaa9f4ffce96232fffd58d6ddbda4a99ce4f8
Gderhy <gabrielderhy@gmail.com>
2025-03-10 17:05:51 -0400

    Refactor database schema and update table creation logic for consistency

--- q1/ddl.sql
+++ q1/ddl.sql
@@ -1,6 +1,5 @@
-CREATE TABLE
-  films (
-    film_id SERIAL PRIMARY KEY,
+CREATE TABLE films (
+    id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     episode_id INT UNIQUE NOT NULL,
     opening_crawl TEXT,
@@ -9,13 +8,12 @@ CREATE TABLE
     release_date DATE,
     created TIMESTAMP,
     edited TIMESTAMP,
-    film_url TEXT
-  );
+    url TEXT
+);
 
-CREATE TABLE
-  planets (
-    planet_id SERIAL PRIMARY KEY,
-    planet_name TEXT NOT NULL,
+CREATE TABLE planets (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
     rotation_period TEXT,
     orbital_period TEXT,
     diameter TEXT,
@@ -23,16 +21,15 @@ CREATE TABLE
     gravity TEXT,
     terrain TEXT,
     surface_water TEXT,
-    planet_population TEXT,
+    population TEXT,
     created TIMESTAMP,
     edited TIMESTAMP,
-    planet_url TEXT
-  );
+    url TEXT
+);
 
-CREATE TABLE
-  people (
-    person_id SERIAL PRIMARY KEY,
-    person_name TEXT NOT NULL,
+CREATE TABLE people (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
     height TEXT,
     mass TEXT,
     hair_color TEXT,
@@ -43,45 +40,41 @@ CREATE TABLE
     homeworld_id INT,
     created TIMESTAMP,
     edited TIMESTAMP,
-    people_url TEXT,
-    FOREIGN KEY (homeworld_id) REFERENCES planets (planet_id) ON DELETE SET NULL
-  );
+    url TEXT,
+    FOREIGN KEY (homeworld_id) REFERENCES planets (id) ON DELETE SET NULL
+);
 
-CREATE TABlE
-  planet_people (
+CREATE TABLE planet_people (
     planet_id INT,
     people_id INT,
     PRIMARY KEY (planet_id, people_id),
-    FOREIGN KEY (planet_id) REFERENCES planets (planet_id) ON DELETE CASCADE,
-    FOREIGN KEY (people_id) REFERENCES people (person_id) ON DELETE CASCADE
-  );
+    FOREIGN KEY (planet_id) REFERENCES planets (id) ON DELETE CASCADE,
+    FOREIGN KEY (people_id) REFERENCES people (id) ON DELETE CASCADE
+);
 
-CREATE TABLE
-  film_people (
+CREATE TABLE film_people (
     people_id INT,
     film_id INT,
     PRIMARY KEY (people_id, film_id),
-    FOREIGN KEY (people_id) REFERENCES people (person_id) ON DELETE CASCADE,
-    FOREIGN KEY (film_id) REFERENCES films (film_id) ON DELETE CASCADE
-  );
+    FOREIGN KEY (people_id) REFERENCES people (id) ON DELETE CASCADE,
+    FOREIGN KEY (film_id) REFERENCES films (id) ON DELETE CASCADE
+);
 
-CREATE TABLE
-  planet_films (
+CREATE TABLE planet_films (
     planet_id INT,
     film_id INT,
     PRIMARY KEY (planet_id, film_id),
-    FOREIGN KEY (planet_id) REFERENCES planets (planet_id) ON DELETE CASCADE,
-    FOREIGN KEY (film_id) REFERENCES films (film_id) ON DELETE CASCADE
-  );
+    FOREIGN KEY (planet_id) REFERENCES planets (id) ON DELETE CASCADE,
+    FOREIGN KEY (film_id) REFERENCES films (id) ON DELETE CASCADE
+);
 
-CREATE TABLE
-  starships (
-    starship_id SERIAL PRIMARY KEY,
-    starship_name TEXT NOT NULL,
+CREATE TABLE starships (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
     model TEXT,
     manufacturer TEXT,
     cost_in_credits TEXT,
-    starship_length TEXT,
+    length TEXT,
     max_atmosphering_speed TEXT,
     crew TEXT,
     passengers TEXT,
@@ -89,71 +82,65 @@ CREATE TABLE
     consumables TEXT,
     hyperdrive_rating TEXT,
     MGLT TEXT,
-    starship_class TEXT,
+    class TEXT,
     created TIMESTAMP,
     edited TIMESTAMP,
-    starship_url TEXT
-  );
+    url TEXT
+);
 
-CREATE TABLE
-  pilots (
+CREATE TABLE pilots (
     starship_id INT,
     people_id INT,
     PRIMARY KEY (starship_id, people_id),
-    FOREIGN KEY (starship_id) REFERENCES starships (starship_id) ON DELETE CASCADE,
-    FOREIGN KEY (people_id) REFERENCES people (person_id) ON DELETE CASCADE
-  );
+    FOREIGN KEY (starship_id) REFERENCES starships (id) ON DELETE CASCADE,
+    FOREIGN KEY (people_id) REFERENCES people (id) ON DELETE CASCADE
+);
 
-CREATE TABLE
-  film_starship (
+CREATE TABLE film_starship (
     film_id INT,
     starship_id INT,
     PRIMARY KEY (film_id, starship_id),
-    FOREIGN KEY (film_id) REFERENCES films (film_id) ON DELETE CASCADE,
-    FOREIGN KEY (starship_id) REFERENCES starships (starship_id) ON DELETE CASCADE
-  );
+    FOREIGN KEY (film_id) REFERENCES films (id) ON DELETE CASCADE,
+    FOREIGN KEY (starship_id) REFERENCES starships (id) ON DELETE CASCADE
+);
 
-CREATE TABLE
-  vehicles (
-    vehicle_id SERIAL PRIMARY KEY,
-    vehicle_name TEXT NOT NULL,
+CREATE TABLE vehicles (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
     model TEXT,
     manufacturer TEXT,
     cost_in_credits TEXT,
-    vehicle_length TEXT,
+    length TEXT,
     max_atmosphering_speed TEXT,
     crew TEXT,
     passengers TEXT,
     cargo_capacity TEXT,
     consumables TEXT,
-    vehicle_class TEXT,
+    class TEXT,
     created TIMESTAMP,
     edited TIMESTAMP,
-    vehicle_url TEXT
-  );
+    url TEXT
+);
 
-CREATE TABLE
-  vehicle_pilots (
+CREATE TABLE vehicle_pilots (
     vehicle_id INT,
     people_id INT,
     PRIMARY KEY (vehicle_id, people_id),
-    FOREIGN KEY (vehicle_id) REFERENCES vehicles (vehicle_id) ON DELETE CASCADE,
-    FOREIGN KEY (people_id) REFERENCES people (person_id) ON DELETE CASCADE
-  );
+    FOREIGN KEY (vehicle_id) REFERENCES vehicles (id) ON DELETE CASCADE,
+    FOREIGN KEY (people_id) REFERENCES people (id) ON DELETE CASCADE
+);
 
-CREATE TABLE
-  film_vehicle (
+CREATE TABLE film_vehicle (
     film_id INT,
     vehicle_id INT,
     PRIMARY KEY (film_id, vehicle_id),
-    FOREIGN KEY (film_id) REFERENCES films (film_id) ON DELETE CASCADE,
-    FOREIGN KEY (vehicle_id) REFERENCES vehicles (vehicle_id) ON DELETE CASCADE
-  );
+    FOREIGN KEY (film_id) REFERENCES films (id) ON DELETE CASCADE,
+    FOREIGN KEY (vehicle_id) REFERENCES vehicles (id) ON DELETE CASCADE
+);
 
-CREATE TABLE
-  species (
-    species_id SERIAL PRIMARY KEY,
-    species_name TEXT NOT NULL,
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
     classification TEXT,
     designation TEXT,
     average_height INT,
@@ -162,18 +149,17 @@ CREATE TABLE
     eye_colors TEXT,
     average_lifespan INT,
     homeworld_id INT,
-    species_language TEXT,
+    language TEXT,
     created TIMESTAMP,
     edited TIMESTAMP,
-    species_url TEXT,
-    FOREIGN KEY (homeworld_id) REFERENCES planets (planet_id) ON DELETE SET NULL
-  );
+    url TEXT,
+    FOREIGN KEY (homeworld_id) REFERENCES planets (id) ON DELETE SET NULL
+);
 
-CREATE TABLE
-  film_species (
+CREATE TABLE film_species (
     film_id INT,
     species_id INT,
     PRIMARY KEY (film_id, species_id),
-    FOREIGN KEY (film_id) REFERENCES films (film_id) ON DELETE CASCADE,
-    FOREIGN KEY (species_id) REFERENCES species (species_id) ON DELETE CASCADE
-  );
+    FOREIGN KEY (film_id) REFERENCES films (id) ON DELETE CASCADE,
+    FOREIGN KEY (species_id) REFERENCES species (id) ON DELETE CASCADE
+);
--- q1/q1_creatingTables.js
+++ q1/q1_creatingTables.js
@@ -1,7 +1,37 @@
 import { fileURLToPath } from "url"; 
 import path from "path";
-import { getPool } from "../db.js";
+import { disconnectDB, getPool } from "../db.js";
 import fs from "fs";
+
+const deleteTables = async (pool) => {
+  try {
+    console.log("Deleting tables...");
+
+    await pool.query(`
+      DROP TABLE IF EXISTS 
+        film_species, 
+        film_vehicle, 
+        vehicle_pilots, 
+        vehicles, 
+        film_starship, 
+        pilots, 
+        starships, 
+        planet_films, 
+        planet_people, 
+        film_people, 
+        people, 
+        species, 
+        planets, 
+        films 
+      CASCADE;
+    `);
+
+    console.log("All tables deleted");
+
+  } catch (err) {
+    console.error("❌ Error during table deletion:", err.stack);
+  }
+};
 
 const createTables = async () => {
   try {
@@ -21,14 +51,19 @@ const createTables = async () => {
     console.log("Creating tables");
     await pool.query(ddl);
     console.log("Tables created");
-
-    // Disconnect from the database
-    console.log("Disconnecting from PostgreSQL");
-    await pool.end();
-    console.log("Disconnected from PostgreSQL");
   } catch (err) {
     console.error("Error during table creation", err.stack);
   }
 };
 
-await createTables();
+
+const main = async () => {
+  const pool = await getPool();
+
+  await deleteTables(pool);
+  await createTables(pool);
+
+  await disconnectDB();
+};
+
+await main();
