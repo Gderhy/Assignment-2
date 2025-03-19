abef4e24c516ed38086732433b30aba4b515b63a
Gderhy <gabrielderhy@gmail.com>
2025-03-18 23:56:49 -0400

    Add movie_id column to normalized_star_wars_popularity table and update data insertion logic

--- q5/tmdb_ddl.sql
+++ q5/tmdb_ddl.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS normalized_star_wars_popularity;
 
 CREATE TABLE normalized_star_wars_popularity(
   film_id INT REFERENCES normalized_films(film_id),
+  movie_id INT,
   popularity REAL
 );
 
--- q5/tmdb_dml.sql
+++ q5/tmdb_dml.sql
@@ -5,7 +5,7 @@ INSERT INTO normalized_films (title)
 SELECT DISTINCT title FROM star_wars_popularity
 ON CONFLICT (title) DO NOTHING;
 
-INSERT INTO normalized_star_wars_popularity (film_id, popularity)
-SELECT nf.film_id, sp.popularity
+INSERT INTO normalized_star_wars_popularity (film_id, movie_id, popularity)
+SELECT nf.film_id, sp.movie_id, sp.popularity 
 FROM star_wars_popularity sp
 JOIN normalized_films nf ON sp.title = nf.title;
