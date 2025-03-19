5c8999e1992140f728ddd16abcc790f4208ea267
Gderhy <gabrielderhy@gmail.com>
2025-03-19 00:18:41 -0400

    Refactor DDL and DML for TMDB data: create tmdb_movie_ids table and update data insertion logic for keywords and popularity

--- q5/tmdb_ddl.sql
+++ q5/tmdb_ddl.sql
@@ -1,21 +1,24 @@
-DROP TABLE IF EXISTS normalized_star_wars_popularity;
 DROP TABLE IF EXISTS normalized_star_wars_keywords;
+
 DROP TABLE IF EXISTS normalized_keywords;
 
-CREATE TABLE normalized_star_wars_popularity(
-  film_id INT REFERENCES normalized_films(film_id) ON DELETE CASCADE,
-  movie_id INT,
-  popularity REAL
-);
+DROP TABLE IF EXISTS tmdb_movie_ids;
 
-CREATE TABLE normalized_keywords(
-  keyword_id SERIAL PRIMARY KEY,
-  keyword TEXT UNIQUE
-);
+CREATE TABLE
+  tmdb_movie_ids (
+    film_id INT PRIMARY KEY REFERENCES normalized_films (film_id) ON DELETE CASCADE,
+    movie_id INT UNIQUE
+  );
 
-CREATE TABLE normalized_star_wars_keywords(
-  film_id INT REFERENCES normalized_films(film_id) ON DELETE CASCADE,
-  keyword_id INT REFERENCES normalized_keywords(keyword_id) ON DELETE CASCADE
-);
+CREATE TABLE
+  normalized_keywords (
+    keyword_id SERIAL PRIMARY KEY,
+    keyword TEXT UNIQUE
+  );
 
-
+CREATE TABLE
+  normalized_star_wars_keywords (
+    film_id INT REFERENCES normalized_films (film_id) ON DELETE CASCADE,
+    keyword_id INT REFERENCES normalized_keywords (keyword_id) ON DELETE CASCADE,
+    PRIMARY KEY (film_id, keyword_id)
+  );
--- q5/tmdb_dml.sql
+++ q5/tmdb_dml.sql
@@ -1,24 +1,37 @@
 -- First insert the movie data from star_wars_popularity into the normalized_films table
--- Then insert the popularity data from star_wars_popularity into the normalized_star_wars_popularity table
+INSERT INTO
+  normalized_films (title)
+SELECT DISTINCT
+  title
+FROM
+  star_wars_popularity ON CONFLICT (title) DO NOTHING;
 
-INSERT INTO normalized_films (title)
-SELECT DISTINCT title FROM star_wars_popularity
-ON CONFLICT (title) DO NOTHING;
-
-INSERT INTO normalized_star_wars_popularity (film_id, movie_id, popularity)
-SELECT nf.film_id, sp.movie_id, sp.popularity 
-FROM star_wars_popularity sp
-JOIN normalized_films nf ON sp.title = nf.title;
+-- Next insert the movie data from star_wars_popularity into the tmdb_movie_ids table
+INSERT INTO
+  tmdb_movie_ids (film_id, movie_id)
+SELECT DISTINCT
+  nf.film_id,
+  swp.movie_id
+FROM
+  normalized_films nf
+  JOIN star_wars_popularity swp ON nf.title = swp.title ON CONFLICT (film_id) DO NOTHING;
 
 -- Next insert the keywords from star_wars_keywords into the normalized_keywords table
 -- Then insert the keyword data from star_wars_keywords into the normalized_star_wars_keywords table
+INSERT INTO
+  normalized_keywords (keyword)
+SELECT DISTINCT
+  keyword
+FROM
+  star_wars_keywords ON CONFLICT (keyword) DO NOTHING;
 
-INSERT INTO normalized_keywords (keyword)
-SELECT DISTINCT keyword FROM star_wars_keywords
-ON CONFLICT (keyword) DO NOTHING;
-
-INSERT INTO normalized_star_wars_keywords (film_id, keyword_id)
-SELECT nswp.film_id, nk.keyword_id
-FROM normalized_star_wars_popularity nswp
-JOIN star_wars_keywords swk ON nswp.movie_id = swk.movie_id
-JOIN normalized_keywords nk ON swk.keyword = nk.keyword;
+INSERT INTO
+  normalized_star_wars_keywords (film_id, keyword_id)
+SELECT
+  nf.film_id,
+  nk.keyword_id
+FROM
+  star_wars_keywords swk
+  JOIN tmdb_movie_ids tmi ON swk.movie_id = tmi.movie_id
+  JOIN normalized_films nf ON tmi.film_id = nf.film_id
+  JOIN normalized_keywords nk ON swk.keyword = nk.keyword;
